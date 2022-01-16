1a6fcda3031c3266dbc68048ae86b16bced9c2fc
adavidmontero <adavidmontero@outlook.com>
2022-01-16 10:03:39 -0500

    Corrección vista principal

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,19 +6,19 @@ const Movie = ({ movie }) => {
     const { id, title, poster_path } = movie;
 
     return ( 
-        <Link
-            to={ `/movies/${id}` }
-        >
-            <div className="col mb-3">
-                    <img 
-                        src={ `https://image.tmdb.org/t/p/w500${poster_path}` } 
-                        className="img-fluid border border-secondary" 
-                        alt={`${title}'s poster`}
-                        style={{ cursor: 'pointer' }}
-                        loading="lazy"
-                    />
-            </div>
-        </Link>
+        <div className="col mb-3">
+            <Link
+                to={ `/movies/${id}` }
+            >
+                <img 
+                    src={ `https://image.tmdb.org/t/p/w500${poster_path}` } 
+                    className="img-fluid border border-secondary" 
+                    alt={`${title}'s poster`}
+                    style={{ cursor: 'pointer' }}
+                    loading="lazy"
+                />
+            </Link>
+        </div>
      );
 }
  
